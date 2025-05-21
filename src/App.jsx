916bc8f3eb0abd86924581923c84480e484b3551
Jawaid Aziz <javaidmemon24@gmail.com>
2025-05-22 01:40:16 +0500

    module2

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [gender, setGender] = useState("");
+  const showFormData = (formData) => {
+    setName(formData.get('name'));
+    setEmail(formData.get('email'));
+    setGender(formData.get('gender'));
+  };
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <form action={showFormData}>
+          <input type="text" name="name" placeholder="your name" />
+          <input type="email" name="email" placeholder="your email" />
+          <label htmlFor="gender">Male</label>
+          <input type="radio" name="gender" value="Male" />
+          <label htmlFor="gender">Female</label>
+          <input type="radio" name="gender" value="Female" />
+          <button type="submit">Show Data</button>
+        </form>
+        <div>
+          Name: {name}
+          Email: {email}
+          Gender: {gender}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
