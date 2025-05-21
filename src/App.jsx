1b899aae3dcc03ff22416b338e3b37efea6ace40
Jawaid Aziz <javaidmemon24@gmail.com>
2025-05-22 02:01:53 +0500

    module3

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,16 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [gender, setGender] = useState("");
-  const showFormData = (formData) => {
-    setName(formData.get('name'));
-    setEmail(formData.get('email'));
-    setGender(formData.get('gender'));
-  };
+  const items = ['item1', 'item2', 'item3', 'item4'];
   return (
     <>
-      <div>
-        <form action={showFormData}>
-          <input type="text" name="name" placeholder="your name" />
-          <input type="email" name="email" placeholder="your email" />
-          <label htmlFor="gender">Male</label>
-          <input type="radio" name="gender" value="Male" />
-          <label htmlFor="gender">Female</label>
-          <input type="radio" name="gender" value="Female" />
-          <button type="submit">Show Data</button>
-        </form>
-        <div>
-          Name: {name}
-          Email: {email}
-          Gender: {gender}
-        </div>
-      </div>
+    <div>
+      <ol>
+        {items.map(item => {
+          return <li>{item}</li>
+        })}
+      </ol>
+    </div>
     </>
   );
 }
